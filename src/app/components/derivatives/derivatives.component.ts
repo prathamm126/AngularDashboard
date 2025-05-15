8173713d003a81f72d2760395a8932c432e5e66e
Pratham <prathamchawlaarora465@gmail.com>
2025-05-15 15:49:06 +0530

    Initial Commit: Angular Project setup

---
+++ src/app/components/derivatives/derivatives.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-derivatives',
+  imports: [],
+  templateUrl: './derivatives.component.html',
+  styleUrl: './derivatives.component.css'
+})
+export class DerivativesComponent {
+
+}
